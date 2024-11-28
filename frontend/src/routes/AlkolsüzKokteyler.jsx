63b31092b8d87f9b4a2f26bd704ea05a0a080bd5
Rahmican Tut <124275194+Revlice@users.noreply.github.com>
2024-11-29 01:43:29 +0300

    Update AlkolsüzKokteyler.jsx

--- frontend/src/routes/AlkolsüzKokteyler.jsx
+++ frontend/src/routes/AlkolsüzKokteyler.jsx
@@ -15,7 +15,7 @@ const AlkolsuzKokteyler = ()=>{
 
     const fetchMenu = async () => {
         try {
-            const response = await axios.get('https://greenbean-coffee.onrender.com/api/menu');
+            const response = await axios.get('https://greenbean-backend.onrender.com/api/menu');
             console.log("Fetched menu items:", response.data);
 
             // "gunebaslarken" kategorisindeki öğeleri filtreleme
